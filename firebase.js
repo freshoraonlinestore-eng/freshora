a5ddc495e458aa0a8e68cca569fc7e53912a8cf5
freshoraonlinestore-eng <freshoraonlinestore@gmail.com>
2026-05-31 19:24:37 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,12 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { 
-  getFirestore, collection, addDoc, getDocs, deleteDoc, doc 
+  getFirestore,
+  collection,
+  onSnapshot,
+  addDoc,
+  getDocs,
+  deleteDoc,
+  doc
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 import {
@@ -14,7 +20,6 @@ const firebaseConfig = {
   apiKey: "AIzaSyBLTQUBX-8S5HnNRJhN_mZ4UMlF6WYlQS0",
   authDomain: "freshora-store-38cef.firebaseapp.com",
   projectId: "freshora-store-38cef",
-  databaseURL: "https://freshora-store-38cef-default-rtdb.asia-southeast1.firebasedatabase.app",
   storageBucket: "freshora-store-38cef.appspot.com",
   messagingSenderId: "836130122991",
   appId: "1:836130122991:web:1b1bfe299cf424e14aff1a1"
@@ -25,4 +30,14 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 
-export { collection, addDoc, getDocs, deleteDoc, doc, signInWithEmailAndPassword, signOut, onAuthStateChanged };
+export {
+  collection,
+  onSnapshot,
+  addDoc,
+  getDocs,
+  deleteDoc,
+  doc,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
+};
