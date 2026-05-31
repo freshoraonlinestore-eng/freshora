9b17695406a9dfa31d72cad6b77e2944a1da321f
freshoraonlinestore-eng <freshoraonlinestore@gmail.com>
2026-05-31 19:12:03 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,33 +1,38 @@
 import {
-  db, collection, addDoc, getDocs, deleteDoc, doc
+  db,
+  collection,
+  addDoc,
+  onSnapshot
 } from "./firebase.js";
 
 const list = document.getElementById("list");
 
+// ADD PRODUCT
 window.addProduct = async () => {
   await addDoc(collection(db, "products"), {
-    name: name.value,
-    price: price.value,
-    image: image.value
+    name: document.getElementById("name").value,
+    price: document.getElementById("price").value,
+    image: document.getElementById("image").value
   });
 
-  load();
+  // clear inputs
+  name.value = "";
+  price.value = "";
+  image.value = "";
 };
 
-async function load() {
-  const snap = await getDocs(collection(db, "products"));
+// REALTIME LOAD (IMPORTANT FIX)
+onSnapshot(collection(db, "products"), (snap) => {
   list.innerHTML = "";
 
-  snap.forEach(d => {
-    const p = d.data();
+  snap.forEach((docItem) => {
+    const p = docItem.data();
 
     list.innerHTML += `
       <div class="card">
         <h3>${p.name}</h3>
-        <p>${p.price}</p>
+        <p>Rs ${p.price}</p>
       </div>
     `;
   });
-}
-
-load();
+});
