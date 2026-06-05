d822cc4b09bd894e0ac99a8281dcce91c8826234
freshoraonlinestore-eng <freshoraonlinestore@gmail.com>
2026-06-05 15:17:14 +0530

    Refactor Firebase imports and configuration

--- firebase.js
+++ firebase.js
@@ -1,48 +1,28 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 import {
-getFirestore,
-collection,
-onSnapshot,
-addDoc,
-getDocs,
-deleteDoc,
-updateDoc,
-doc
+  getFirestore,
+  collection,
+  onSnapshot,
+  addDoc
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-import {
-getAuth,
-signInWithEmailAndPassword,
-signOut,
-onAuthStateChanged
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
 const firebaseConfig = {
-apiKey: "AIzaSyBLTQUBX-8S5HnNRJhN_mZ4UMlF6WYlQS0",
-authDomain: "freshora-store-38cef.firebaseapp.com",
-databaseURL: "https://freshora-store-38cef-default-rtdb.asia-southeast1.firebasedatabase.app",
-projectId: "freshora-store-38cef",
-storageBucket: "freshora-store-38cef.firebasestorage.app",
-messagingSenderId: "836130122991",
-appId: "1:836130122991:web:31a2ea12dce7c5a44aff1a",
-measurementId: "G-JSZYZF9EW0"
+  apiKey: "AIzaSyBLTQUBX-8S5HnNRJhN_mZ4UMlF6WYlQS0",
+  authDomain: "freshora-store-38cef.firebaseapp.com",
+  projectId: "freshora-store-38cef",
+  storageBucket: "freshora-store-38cef.appspot.com",
+  messagingSenderId: "836130122991",
+  appId: "1:836130122991:web:31a2ea12dce7c5a44aff1a"
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+const db = getFirestore(app);
 
 export {
-collection,
-onSnapshot,
-addDoc,
-getDocs,
-deleteDoc,
-updateDoc,
-doc,
-signInWithEmailAndPassword,
-signOut,
-onAuthStateChanged
+  db,
+  collection,
+  onSnapshot,
+  addDoc
 };
