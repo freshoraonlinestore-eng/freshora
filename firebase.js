374c840ae28e38192f8cde5eb1b385d382d025b7
freshoraonlinestore-eng <freshoraonlinestore@gmail.com>
2026-06-10 16:40:34 +0000

    Fix export syntax in firebase.js

--- firebase.js
+++ firebase.js
@@ -11,6 +11,7 @@ import {
   orderBy
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+// Firebase Config
 const firebaseConfig = {
   apiKey: "AIzaSyBLTQUBX-8S5HnNRJhN_mZ4UMlF6WYlQS0",
   authDomain: "freshora-store-38cef.firebaseapp.com",
@@ -20,11 +21,15 @@ const firebaseConfig = {
   appId: "1:836130122991:web:31a2ea12dce7c5a44aff1a"
 };
 
+// Initialize App
 const app = initializeApp(firebaseConfig);
 
+// Firestore DB
 const db = getFirestore(app);
 
+// Export everything needed
 export {
+  app,
   db,
   collection,
   onSnapshot,
