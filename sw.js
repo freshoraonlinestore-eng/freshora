c53f2cdad7ce6a34e354de1206993cf0af2c1602
freshoraonlinestore-eng <freshoraonlinestore@gmail.com>
2026-06-11 12:29:18 +0530

    Implement service worker with caching functionality

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = "freshora-cache-v1";
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.map((key) => caches.delete(key)));
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request));
+});
