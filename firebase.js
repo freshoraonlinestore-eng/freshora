8c5a0a56a2c1b5a1fb0bce37e47d7b3178558634
freshoraonlinestore-eng <freshoraonlinestore@gmail.com>
2026-06-14 16:55:03 +0530

    Add Firestore auth and deleteDoc functionality

--- firebase.js
+++ firebase.js
@@ -1,5 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
+/* =========================
+FIRESTORE
+========================= */
 import {
   getFirestore,
   collection,
@@ -8,10 +11,21 @@ import {
   getDocs,
   query,
   where,
-  orderBy
+  orderBy,
+  deleteDoc,
+  doc
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// Firebase Config
+/* =========================
+AUTH (ADMIN LOGIN)
+========================= */
+import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+/* =========================
+FIREBASE CONFIG
+========================= */
 const firebaseConfig = {
   apiKey: "AIzaSyBLTQUBX-8S5HnNRJhN_mZ4UMlF6WYlQS0",
   authDomain: "freshora-store-38cef.firebaseapp.com",
@@ -21,21 +35,33 @@ const firebaseConfig = {
   appId: "1:836130122991:web:31a2ea12dce7c5a44aff1a"
 };
 
-// Initialize App
+/* =========================
+INIT APP
+========================= */
 const app = initializeApp(firebaseConfig);
 
-// Firestore DB
+/* =========================
+SERVICES
+========================= */
 const db = getFirestore(app);
+const auth = getAuth(app);
 
-// Export everything needed
+/* =========================
+EXPORT
+========================= */
 export {
   app,
   db,
+  auth,
+
+  // firestore
   collection,
   onSnapshot,
   addDoc,
   getDocs,
   query,
   where,
-  orderBy
+  orderBy,
+  deleteDoc,
+  doc
 };
